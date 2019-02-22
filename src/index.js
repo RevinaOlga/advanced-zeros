75dfb55d9c9831b6881c2e44ae01eaac5cfa7749
Olga <revina.volga@gmail.ru>
2019-02-22 20:34:39 +0300

    add new algorithm index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,44 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
-  var divider = Math.floor(number / maxDivide(base)),
-  result = divider;
 
-  while (divider > 0) {
-    divider = Math.floor(divider / maxDivide(base));
-    result += divider;
+  var arr = divider(base);
+
+  var divide = Math.floor(number / arr[arr.length - 1]);
+  var sum = divide;
+  var i = 2;
+  
+  while (divide > 0) {
+    divide = Math.floor(number / Math.pow(arr[arr.length - 1], i));
+    sum += divide;
+    i++;
   }
 
-  return result;
+  return Math.floor(sum/powerDigit(base, arr[arr.length - 1]));
 
-  function maxDivider(num) {
-    for (var i = 3; i < num; i++) {
-      if (num % i == 0 && simpleDigit(i)) {
-        return i;
-      }
+  function powerDigit(num, prime) {
+    var i = 0; 
+    while (num % prime == 0) {
+        num = num / prime;
+        i++;
     }
+    return i;
+  }
+
+  function divider(num) {
+    var arr = [];
+    for (var i = 2; i <= num; i++) {
+        if (num % i == 0 && simpleDigit(i)) {
+            arr.push(i);
+        }
+    }
+    return arr;
   }
 
   function simpleDigit(num) {
-    for (var i = 2; i < num; i++) {
-      if (num % i == 0) {
-        return false;
-      }
+    for (var i = 2; i < num -1; i++) {
+        if (num % i == 0) {
+            return false;
+        }
     }
     return true;
   }
