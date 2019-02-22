cda27f94edef3662b4533e662b39edf07c053568
Olga <revina.volga@gmail.ru>
2019-02-22 10:40:43 +0300

    add task advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  var divider = Math.floor(number / maxDivide(base)),
+  result = divider;
+
+  while (divider > 0) {
+    divider = Math.floor(divider / maxDivide(base));
+    result += divider;
+  }
+
+  return result;
+
+  function maxDivider(num) {
+    for (var i = 3; i < num; i++) {
+      if (num % i == 0 && simpleDigit(i)) {
+        return i;
+      }
+    }
+  }
+
+  function simpleDigit(num) {
+    for (var i = 2; i < num; i++) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
